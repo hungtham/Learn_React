1eea96a18262bbb9da7f7f2d55a0e392b5a3a762
hunggtham <hunggtham@gmail.com>
2022-05-20 16:39:31 +0900

    dom event

--- src/views/example/Mycomponent.js
+++ src/views/example/Mycomponent.js
@@ -12,6 +12,10 @@ class Mycomponent extends React.Component {
             name: event.target.value
         })
     }
+
+    handleClickButton = () => {
+        alert("Clicked");
+    }
     render() {
         let name = 'Eric';
         return (
@@ -22,13 +26,15 @@ class Mycomponent extends React.Component {
                     <input value={this.state.name} type="text"
                         onChange={(event) => this.handleOnChangeName(event)} />
                     {console.log('My name is', name)}
-                    Hello my component, my name is {this.state['name']}
+                    Hello everyone, my name is {this.state['name']}
                 </div>
                 <div className="Second">
                     My school is: {this.state.school}
                 </div>
+                <div className="third">
+                    <button onClick={() => this.handleClickButton()}>Click Me!!</button>
+                </div>
             </div>
-
         )
 
     }
