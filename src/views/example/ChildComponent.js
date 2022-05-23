9dca5a999338304e0134192bf5e40b42b09aa3a8
hunggtham <hunggtham@gmail.com>
2022-05-24 01:49:49 +0900

    learn nesting component with props

---
+++ src/views/example/ChildComponent.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+class ChildComponent extends React.Component {
+
+    render() {
+        return (
+            <>
+                <div>
+                    ChildComponent: {this.props.name}
+                </div>
+
+            </>
+        )
+
+    }
+}
+export default ChildComponent;
--- src/views/example/Mycomponent.js
+++ src/views/example/Mycomponent.js
@@ -1,40 +1,52 @@
 
 import React from "react";
-
+import ChildComponent from "./ChildComponent";
 class Mycomponent extends React.Component {
-    state = {
-        name: 'Hieu',
-        school: 'Ajou',
-    }
 
-    handleOnChangeName = (event) => {
+    state = {
+        firstName: '',
+        lastName: '',
+
+    };
+    handleChangeFirstName = (event) => {
         this.setState({
-            name: event.target.value
+            firstName: event.target.value
         })
     }
-
-    handleClickButton = () => {
-        alert("Clicked");
+    handleChangeLastName = (event) => {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        alert('FirstName:' + this.state.firstName + '\nLastName' + this.state.lastName);
     }
     render() {
-        let name = 'Eric';
+        console.log('>>>>>>Render : ', this.state);
         return (
-
-            <div className="parent">
-
-                <div className="First">
-                    <input value={this.state.name} type="text"
-                        onChange={(event) => this.handleOnChangeName(event)} />
-                    {console.log('My name is', name)}
-                    Hello everyone, my name is {this.state['name']}
-                </div>
-                <div className="Second">
-                    My school is: {this.state.school}
-                </div>
-                <div className="third">
-                    <button onClick={() => this.handleClickButton()}>Click Me!!</button>
-                </div>
-            </div>
+            <>
+                <form >
+                    <label htmlFor="fname">First name:</label><br />
+                    <input
+                        type="text"
+                        value={this.state.firstName}
+                        onChange={(event) => this.handleChangeFirstName(event)}
+                    /><br />
+                    <label htmlFor="lname">Last name:</label><br />
+                    <input
+                        type="text"
+                        value={this.state.lastName}
+                        onChange={(event) => this.handleChangeLastName(event)} /><br />
+                    <input type="button"
+                        value="Submit"
+                        onClick={(event) => this.handleSubmit(event)}
+                    />
+                </form>
+                <ChildComponent name={'ChildComponent 1'} />
+                <ChildComponent name={'ChildComponent 2'} />
+                <ChildComponent name={'ChildComponent 3'} />
+            </>
         )
 
     }
